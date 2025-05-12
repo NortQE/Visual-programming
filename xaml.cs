28530b2d12740641a4a89411f2df55f4b147f575
ARTEM SOKOL <sokol999i8@gmail.com>
2025-05-12 22:11:04 +0300

    Create xaml.cs

---
+++ xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+
+namespace LoginApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
